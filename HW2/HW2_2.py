27b3f5376a8ea404ca08777612e06a82f1d96ccf
Eugene Yang <eugene87222@gmail.com>
2021-04-13 10:58:24 +0800

    upload task3 result, change output name

--- HW2/HW2_2.py
+++ HW2/HW2_2.py
@@ -103,4 +103,5 @@ if __name__ == '__main__':
         if i == args.num_layers-1:
             plt.gca().text(1.3, 0.5, 'Laplacian', transform=plt.gca().transAxes, **text_kwargs)
 
-    plt.savefig(os.path.join('task2_result', f'{img_name}_pyramid.png'), dpi=300)
+    plt.tight_layout()
+    plt.savefig(os.path.join('task2_result', f'{img_name}_pyd_{args.num_layers}_size_{args.filter_size}_sigma_{args.filter_sigma}.png'), dpi=300)
