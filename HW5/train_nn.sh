6bf49024ec7cd5e859a9ba868e477b024a4a7f21
Eugene Yang <eugene87222@gmail.com>
2021-08-25 20:43:22 +0800

    remove shell script

--- HW5/train_nn.sh
+++
@@ -1,6 +0,0 @@
-python main.py --img_size 128 --generate_exp_name --cls_mode nn --repr_mode nn --epoch 200
-python main.py --img_size 128 --generate_exp_name --cls_mode nn --repr_mode nn --epoch 200 --pretrained
-python main.py --img_size 128 --generate_exp_name --cls_mode nn --repr_mode nn --epoch 200 --nn_model_name resnet34
-python main.py --img_size 128 --generate_exp_name --cls_mode nn --repr_mode nn --epoch 200 --pretrained --nn_model_name resnet34
-python main.py --img_size 128 --generate_exp_name --cls_mode nn --repr_mode nn --epoch 200 --nn_model_name resnet50
-python main.py --img_size 128 --generate_exp_name --cls_mode nn --repr_mode nn --epoch 200 --pretrained --nn_model_name resnet50
